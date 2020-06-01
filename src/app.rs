84aac8b5a77c00f35c74abbd3e0a0fecf1e0dc9c
uzushino <torattoria.mamiya@gmail.com>
2020-06-02 00:47:47 +0900

    many0 => many1

--- src/app.rs
+++ src/app.rs
@@ -10,7 +10,7 @@ use nom::{
     bytes::complete::{
         tag,
     },
-    multi::{ many0, many_till }
+    multi::{ many0, many1, many_till }
 };
 
 use crate::parser;
@@ -39,10 +39,10 @@ pub fn make_combinator<'a>() -> impl Fn(Vec<parser::Node>, &'a str) -> IResult<&
         let mut h: BTreeMap<String, String> = BTreeMap::default();
         
         for (idx, token) in tokens.iter().enumerate() {
-
             match token {
                 parser::Node::Lit(a, b, c) => {
                     let a: IResult<&str, &str> = tag(&format!("{}{}{}", a, b, c)[..])(input);
+
                     match a {
                         Ok((rest, _b)) => input = rest,
                         _ => {
@@ -51,10 +51,9 @@ pub fn make_combinator<'a>() -> impl Fn(Vec<parser::Node>, &'a str) -> IResult<&
                         }
                     }
                 },
-            
                 parser::Node::Expr(_, parser::Expr::Var(key)) => {
                     let next = tokens.get(idx + 1);
-
+                    
                     if let Some(parser::Node::Lit(a, b, c)) = next {
                         let result: IResult<&'a str, &'a str> = 
                             take_until(&format!("{}{}{}", a, b, c)[..])(input);
@@ -64,9 +63,13 @@ pub fn make_combinator<'a>() -> impl Fn(Vec<parser::Node>, &'a str) -> IResult<&
                         }
                     } else {
                         let result: IResult<&'a str, &'a str> = take_until("\n")(input);
+
                         if let Ok((rest, capture))  = result {
                             h.insert(key.to_string(), capture.to_string());
                             input = rest;
+                        } else {
+                            h.insert(key.to_string(), input.to_string());
+                            input = "";
                         }
                     }
                 },
@@ -93,6 +96,7 @@ pub fn make_combinator<'a>() -> impl Fn(Vec<parser::Node>, &'a str) -> IResult<&
             }   
         };
         
+                        dbg!((&input, &h));
         IResult::Ok((input, h))
     }
 }
@@ -141,27 +145,34 @@ impl App {
                 let parsed = match tok {
                     Token::Many(t) => {
                         let comb= Self::build(vec![*t.clone()]);
-                        let (rest, result) = many0(comb)(text.trim()).unwrap();
-                        
-                        let a: Vec<BTreeMap<String, String>> = result
-                            .iter()
-                            .flatten()
-                            .map(|s| s.clone())
-                            .collect::<Vec<_>>();
+                        match many1(comb)(text.trim()) {
+                            Ok((rest, result)) => {
+                                let a: Vec<BTreeMap<String, String>> = result
+                                    .iter()
+                                    .flatten()
+                                    .map(|s| s.clone())
+                                    .collect::<Vec<_>>();
 
-                        Some((rest, a.clone()))
-                    }
-                    Token::Skip => {
-                        let remain = &old[(i+1)..old.len()];
-                        let acc = Self::build(remain.to_vec());
-                        let r = 
-                            many_till(anychar,  acc)(text);
-                        match r {
-                            Ok((_rest, (matches, _))) => {
-                                Some((&text[matches.len()..], Vec::default()))
+                                Some((rest, a.clone()))
                             },
                             _ => None
                         }
+                    }
+                    Token::Skip => {
+                        let remain = &old[(i+1)..(i+2)];
+                        let acc = Self::build(remain.to_vec());
+                        let mut result = None;
+
+                        for (u, _ch) in text.chars().into_iter().enumerate() {
+                            let s = &text[u..];
+                            
+                            if let Ok((_rest, _rows)) = acc(s) {
+                                result = Some((s, Vec::default()));
+                                break ;
+                            }
+                        }
+                        
+                        result
                     }
                     Token::While(t) => {
                         let acc = Self::build(vec![*t.clone()]);
@@ -176,9 +187,9 @@ impl App {
                         let (_, tbl) = parser::parse_template(tag.as_bytes(), &syn).unwrap();
                         let comb = make_combinator();
                         let r = comb(tbl, text);
+
                         match r {
                             Ok((rest, value)) => {
-                        
                                 if value.is_empty() {
                                     Some((rest, Vec::default()))
                                 } else {
@@ -195,6 +206,7 @@ impl App {
                         if !tables.is_empty() {
                             results.append(&mut tables);
                         }
+
                         text = rest;
                     }
                     _ => { 
@@ -227,7 +239,7 @@ csv:
       skip:
     - 
       many: 
-        tag: "hoge,{{i}},{{n}},{{a}},{{e}}"
+        tag: "{{i}},{{n}},{{a}},{{e}}\n"
     -
       skip:
     - 
@@ -237,8 +249,8 @@ csv:
         let input = r#"
 id,name,age,email
 ==
-hoge,1,2,3,4
-hoge,5,6,7,8
+1,2,3,4
+5,6,7,8
 ------
 total,20
 "#;
