4eeab3bb7dd1dcc40da1fb33521f471879070c24
uzushino <torattoria.mamiya@gmail.com>
2020-07-16 21:56:03 +0900

    remove unuse command.

--- src/main.rs
+++ src/main.rs
@@ -12,45 +12,6 @@ struct Opt {
     pub file: String,
 }
 
-/*
-fn parse_input(ap: &app::App) -> Option<String> {
-    let mut input = String::default();
-    let _ = io::stdin().read_line(&mut input).ok();
-    let mut result = String::default();
-    let head = if let Some(ref condition) = ap.conditions {
-        app::App::build(vec![condition.start.clone()])
-    } else {
-        app::App::build(vec![ap.templates[0].clone()])
-    };
-
-    if head(input.as_str()).is_ok() {
-        if ap.templates.len() == 1 {
-            return Some(input.clone());
-        }
-
-        result = input.clone();
-
-        loop {
-            let mut buf = String::default();
-            let _ = io::stdin().read_line(&mut buf);
-            let combinator = if let Some(ref condition) = ap.conditions {
-                app::App::build(vec![condition.end.clone()])
-            } else {
-                app::App::build(vec![ap.templates[0].clone()])
-            };
-            let r = combinator(buf.as_str());
-            
-            result = format!("{}{}", result, buf);
-            if r.is_ok() {
-                break;
-            }
-        }
-    }
-
-    Some(result)
-}
-*/
-
 fn main() -> anyhow::Result<()> {
     env_logger::init();
 
@@ -59,7 +20,6 @@ fn main() -> anyhow::Result<()> {
     debug!("{}", contents);
     let config: app::AppConfig = serde_yaml::from_str(&contents)?;
     let app = app::App::new_with_config(&config)?; 
-
     let stdin = std::io::stdin();
 
     loop {
